53279f06f6d8a6e26eacefd8f200679703f50ce5
Newzie801 <thburnham@protonmail.com>
2020-05-03 04:16:32 -0600

    added automatic message cycler

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ var messages = [
     'hello_world',
 ];
 var currentMessageIndex = 3;
-textOutputElement.innerHTML = messages[currentMessageIndex];
+var updateMessageByIndex = function (index) {
+    textOutputElement.innerHTML = messages[index];
+};
+var cycleMessages = function () {
+    var messageListLength = messages.length;
+    var nextMessageIndex = (currentMessageIndex + 1) % messageListLength;
+    currentMessageIndex = nextMessageIndex;
+    updateMessageByIndex(currentMessageIndex);
+};
+setInterval(
+    cycleMessages,
+    1000,
+);
