806ced2eb54f73d77717d7195de202cbaa48eac2
Newzie801 <thburnham@protonmail.com>
2020-05-04 22:30:10 -0600

    converted text cycling to image slideshow

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var textOutputElement = document.getElementById('textOutput');
+var imageOutputElement = document.getElementById('imageOutput');
 var messages = [
-    'Bob',
-    'goat',
-    'learn_to_code',
-    'hello_world',
+    'https://placekitten.com/g/300/300',
+    'https://placekitten.com/g/301/300',
+    'https://placekitten.com/g/302/300',
+    'https://placekitten.com/g/303/300',
+    'https://placekitten.com/g/304/300',
+    'https://placekitten.com/g/305/300',
+    
 ];
 var currentMessageIndex = 3;
 var updateMessageByIndex = function (index) {
     textOutputElement.innerHTML = messages[index];
+    imageOutputElement.src = messages[index];
 };
 var cycleMessages = function () {
     var messageListLength = messages.length;
